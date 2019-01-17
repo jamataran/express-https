f8467c742a293b9fdf6d216292d6771c6c2e236b
jose <jamataran@gmail.com>
2019-01-17 12:47:33 +0100

    Fix

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const app = express();
 const app2= express();
 const fs = require('fs');
 const https = require('https');
 
-// Uso
+// Configuración de los certificados.
 const sslOptionsCert = {
     key: fs.readFileSync("./cert/server.key"),
     cert: fs.readFileSync("./cert/server.cer")
 };
 
+// Servidor y mapeos.
 https.createServer(sslOptionsCert, app2).listen(443);
 app2.get('', (req, res) => res.send('Hello SSL!'));
